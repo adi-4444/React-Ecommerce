ff38f5771d3d8e927de8e1af90d0acc7ee2d0820
Adi <adinarayana4444@gmail.com>
2022-10-09 22:58:12 +0530

    React Ecommerce App

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 function Login() {
+  const [showSignup,setshowSignup] = useState(true);
+  const toggleHandler = () => {
+    setshowSignup(!showSignup)
+  }
+
   return (
     <div className='login-body'>
         <h1>Welcome to Online Shopping</h1>
       <div className="login-wrapper">
 
         <div className="login-form-group">
-          <h2>Login</h2>
-          <div className="username form-group">
-            <input type="text" class="form-control" name='username' placeholder="username" autoFocus required/>
-            <label class="form-label" for='username'>Username</label>
+          <h2>{showSignup ? "Signup" : "Login"}</h2>
+          <div className="form-group">
+            <input type="text" class="form-control" name='email' placeholder="Email" autoFocus required/>
+            <label className='form-label' for='email'>Email</label>
           </div>
+          {
+            showSignup && 
+            <div className="form-group">
+              <input type="text" class="form-control" name='name' placeholder="Name" required/>
+              <label className='form-label' for='name'>Name</label>
+            </div>
+          }
 
-          <div className="password form-group">
-            <input type="password" class="form-control" name='password' placeholder="password" required/>
-            <label class="form-label" for="password">Password</label>
+          <div className="form-group">
+            <input type="password" class="form-control" name='password' placeholder="Password" required/>
+            <label className='form-label' for="password">Password</label>
           </div>
+          {
+            showSignup && 
+            <div className="form-group">
+             <input type="password" class="form-control" name='confirmpassword' placeholder="ConfirmPassword" required/>
+              <label className='form-label' for="confirmpassword">Confirm Password</label>
+            </div>
+          }
           <div className="login-btn">
-            <button>Login</button>
-            <p>Don't have an account ? <a href='/'>Sign Up</a></p>
+            <button>{showSignup ?  "Signup": "Login"}</button>
+            {
+              showSignup ? <p>Already have an account ? <a onClick={toggleHandler}>Login</a></p>
+              :
+              <p> Don't have an account ? <a onClick={toggleHandler}>Signup</a></p>
+            }
+            
           </div>
         </div>
 
