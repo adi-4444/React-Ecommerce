91dff8d4b875df9c7efe0d921b0f0764da28f81f
Adi <adinarayana4444@gmail.com>
2022-10-11 00:02:21 +0530

    React Ecommerce App

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -29,27 +29,27 @@ function Signup() {
           <h2>Signup</h2>
 
             <div className="form-group">
-              <input type="text" class="form-control" name='name' placeholder="Name" autoFocus required
+              <input type="text" className='form-control' name='name' placeholder="Name" autoFocus required
               value={name} onChange={(e)=> setName(e.target.value)}/>
-              <label className='form-label' for='username'>Name *</label>
+              <label className='form-label'>Name *</label>
             </div>
 
           <div className="form-group">
-            <input type="text" class="form-control" name='email *' placeholder="Email" required
+            <input type="text" className='form-control' name='email *' placeholder="Email" required
             value={email} onChange={(e)=> setEmail(e.target.value)}/>
-            <label className='form-label' for='email'>Email *</label>
+            <label className='form-label'>Email *</label>
           </div>
 
           <div className="form-group">
-            <input type="password" class="form-control" name='password' placeholder="Password" required
+            <input type="password" className='form-control' name='password' placeholder="Password" required
             value={password} onChange={(e)=> setPassword(e.target.value)}/>
-            <label className='form-label' for="password">Password *</label>
+            <label className='form-label'>Password *</label>
           </div>
 
             <div className="form-group">
-             <input type="password" class="form-control" name='confirmpassword' placeholder="ConfirmPassword" required
+             <input type="password" className='form-control' name='confirmpassword' placeholder="ConfirmPassword" required
              value={confirmpassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
-              <label className='form-label' for="confirmpassword">Confirm Password *</label>
+              <label className='form-label'>Confirm Password *</label>
               {
                 password && confirmpassword ? 
                 password === confirmpassword ? 
@@ -60,7 +60,7 @@ function Signup() {
             </div>
 
           <div  className="signup-btn">
-            <button type='submit'>Signup</button>
+            <button type='submit' onSubmit={signupHandler}>Signup</button>
             <p>Already have an account ? <a>Login</a></p>
           </div>
 
